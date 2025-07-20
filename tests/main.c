f09bc54f1ac2db22199843fbaa434c3c4447d150
dromi <dromi@proton.me>
2025-07-20 19:25:22 +0200

    Just thinking | tests/

--- tests/main.c
+++ tests/main.c
@@ -12,6 +12,8 @@
 // gekommen. Sonst wäres es noch komplizierter.
 #define strlenv(x) j
 
+#define MAX(x, y)  (((x) > (y)) ? (x) : (y))
+
 struct Sarr {
     char arr[];
 };
@@ -79,7 +81,7 @@ void s_string_concat(char* buff, char* data[]) {
 }
 
 
-void s_combinate() {
+void stack_main_combinate() {
     char* arr[] = {"Hallo, ", "Welt", "My!", NULL};
     char* buff = alloca(getLen(arr));
     s_string_concat(buff, arr);
@@ -89,27 +91,63 @@ void s_combinate() {
 
 }
 
-void combinate() {
+void heap_main_combinate() {
     char* result = string_concat(NULL, "Hallo, ", "Welt!", "My!", NULL);
     printf("%s\n", result); // → "Hallo, Welt!"
     free(result);
     // das erste Argument muss wegen va_args null sein. Aber verbesserungsfähig.
 }
 
+int int_strdiff(char* a, char* b) {
+    int len_a = strlen(a);
+    int len_b = strlen(b);
+    return abs(len_a - len_b);
+    // if it's return 0, there are no changes needed.
+}
 
-void
-string_replace(char* buff,
+void string_replace(char* buff,
     const char* str, const char* target, const char* replacement) {
 
     int s = strlen(str);
 
     // the amount of targets can't be bigger than the size of the string
     // For simplicity, i take the half.
-    char* target_s[s/2];
+    char target_s[s/2];
+    int count = 0;
     
-    for (int i = 0; i < ; i++) {
-        if (
+    for (int i = 0; i < s; i++) {
+        if (str[i] == target)
+            target_s[count++] = i;
     }
+
+    
+    // Prove that I'm overthinking:
+
+    // My first approach is to split strings between the target's.
+    // If the target is more than one char,
+    // then it will check iteratively each char e.g:
+    // R - Ru - Rus - Rust
+    //
+    // everything is a target   -> 0 pieces
+    // every second(2) is a target -> (s/2) pieces
+    // every x is a target -> (s/x) pieces
+    // xxRustxxxRustxxxxRustxxxRust
+    // -> 8 pieces -> 8-1 cut-lines
+    
+    // Second thought is that the string will only be splitten after the target.
+
+    // difference between split everything at once, and step by step?
+    // I'll try the second first.
+
+    // how now I imagine the process:
+    // Ich mag C, weil C schnell ist.
+    // Ich mag C<,> weil C schnell ist. // target_s[0]+1
+    // Ich mag C<,>
+    // Now due to the string-amount with more > 1 , the indecies are no longer valid
+    // So -> calculate the diff between the *replacement* and *target*
+    // Ich mag Rust<,> // stringdiff("Rust"-"C") -> 3
+    // So -> set all indecies target_s[1..x] to +3
+
 
 }
 
